4824a8a75fefbe522d36e5eb45a9d8b5d0f1c505
Dietmar Temme <d.temme@anatelos.de>
2016-03-26 16:14:35 +0100

    fixed file handling

---
+++ src/main/java/de/anatelos/maven/plugins/Xsd2ThriftMojo.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package de.anatelos.maven.plugins;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+//import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import com.github.tranchis.xsd2thrift.Xsd2Thrift;
+import com.github.tranchis.xsd2thrift.Xsd2ThriftException;
+
+/**
+ * The xsd2thrift-maven-plugin creates
+ * <ul>
+ * <li>Google protocol buffer descriptions (.proto files)</li>
+ * <li>Apache Thrift descriptions (.thrift files)</li>
+ * </ul>
+ * from a list of xschema files
+ *
+ * 
+ * @author marug
+ * 
+ */
+@Mojo(name = "parsexsd", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class Xsd2ThriftMojo extends AbstractMojo {
+
+	@Parameter(property = "parsexsd.xschemaList")
+	private List<String> xschemaList;
+	@Parameter(property = "parsexsd.outputDir", defaultValue = "target/generated-sources/proto")
+	private String outputDir;
+	@Parameter(property = "parsexsd.packageName")
+	private String packageName;
+	@Parameter(defaultValue = "protobuf")
+	private String protocol;
+	@Parameter
+	private boolean nestEnums = true;
+
+	private String fileSuffix;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		this.checkFileSuffix();
+		try {
+			Xsd2Thrift xsd2thrift = new Xsd2Thrift(null, packageName, protocol, nestEnums);
+
+			for (String xschema : xschemaList) {
+				String outputFile = outputDir + File.separator
+				        + xschema.substring(xschema.lastIndexOf("/") + 1, xschema.indexOf(".xsd")) + fileSuffix;
+				xsd2thrift.parseXsd(xschema, outputFile);
+			}
+		} catch (Xsd2ThriftException e) {
+			throw new MojoExecutionException(e.getMessage(), null);
+		}
+
+	}
+
+	private void checkFileSuffix() {
+		if (null == fileSuffix) {
+			fileSuffix = "protobuf".equals(protocol) ? ".proto"
+			        : ("thrift".equals(protocol) ? ".thrift" : ("protobuf3".equals(protocol) ? ".proto" : ".bug"));
+		}
+	}
+}
